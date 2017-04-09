2e8a77d5cdddbb180b731a4de4e284e6ab77610c
Ian <keitaro_ayanokoji@yahoo.ie>
2017-04-09 15:11:39 +0100

    Cleanup

--- NetworkRequest/network.h
+++ NetworkRequest/network.h
@@ -1,7 +1,7 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
-#define USER_API "http://10.50.19.19/UserService/rest/user/"
-#define PAYMENT_API "http://10.50.19.19/PaymentService/rest/payment/"
+#define USER_API "http://127.0.0.1/UserService/rest/user/"
+#define PAYMENT_API "http://127.0.0.1/PaymentService/rest/payment/"
 
 #endif // NETWORK_H
